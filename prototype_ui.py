1cfa4976259c0ea7e1202c1c452e2a1403880424
Angel Vazquez <aavazquezs@gmail.com>
2025-10-12 16:27:16 +0200

    feat

--- prototype_ui.py
+++ prototype_ui.py
@@ -7,14 +7,16 @@ if "num_modelos" not in st.session_state:
 def actualizar_num_modelos():
     st.session_state.num_modelos = st.session_state.input_num_modelos
 
-st.sidebar.number_input(
-    "Cantidad de modelos",
-    min_value=1,
-    max_value=100,
-    value=st.session_state.num_modelos,
-    step=1,
-    key="input_num_modelos",  # clave para identificar el widget
-    on_change=actualizar_num_modelos  # se llama cuando cambia
-)
+with st.sidebar:
+    st.title("Configuración")
+    st.number_input(
+        "Cantidad de modelos",
+        min_value=1,
+        max_value=100,
+        value=st.session_state.num_modelos,
+        step=1,
+        key="input_num_modelos",  # clave para identificar el widget
+        on_change=actualizar_num_modelos  # se llama cuando cambia
+    )
 
 st.write(f"Modelos configurados: {st.session_state.num_modelos}")
