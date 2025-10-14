3e72ce910f62c99d57acb39d02a8d4431eda2b2e
Angel Vazquez <aavazquezs@gmail.com>
2025-10-14 22:09:08 +0200

    (feat) interfaz que todavia no es funcional

--- prototype_ui.py
+++ prototype_ui.py
@@ -1,17 +1,34 @@
 import streamlit as st
 import pandas as pd
 from io import StringIO
+from settings import Model, DeepSurvModel, LifelinesCoxPHModel, CoxCCModel, CoxTimeModel
 
 # Inicializar
 if "num_modelos" not in st.session_state:
-    st.session_state.num_modelos = 1
+    st.session_state.num_modelos = 3
 
 if 'input_dataset' not in st.session_state:
     st.session_state.input_dataset = None
 
+#if 'models' not in st.session_state:
+st.session_state.models = []
+
 def actualizar_num_modelos():
     st.session_state.num_modelos = st.session_state.input_num_modelos
 
+def model_factory(model_type)-> Model| None:
+    if model_type == "DeepSurv":
+        return DeepSurvModel()
+    elif model_type == "Lifelines-PHCox":
+        return LifelinesCoxPHModel()
+    elif model_type == "CoxCC":
+        return CoxCCModel()
+    elif model_type == "CoxTime":
+        return CoxTimeModel()
+    else:
+        return None
+
+# Configuracion visual del prototipo
 with st.sidebar:
     st.title("Configuración")
     st.number_input(
@@ -23,11 +40,19 @@ with st.sidebar:
         key="input_num_modelos",  # clave para identificar el widget
         on_change=actualizar_num_modelos  # se llama cuando cambia
     )
+    st.markdown("---")
+    st.markdown("### Modelos para usar")
+    model_types = st.multiselect(
+        "Selecciona los tipos de modelos a usar",
+        options=["Lifelines-PHCox", "DeepSurv", "CoxCC", "CoxTime"],
+        default=["Lifelines-PHCox", "DeepSurv", "CoxCC", "CoxTime"]
+    )
 
 st.title("Prototipo")
 st.write(f"Modelos a configurar: {st.session_state.num_modelos}")
 
-set.header("Cargar Dataset")
+st.markdown("## Cargar Dataset")
+
 # file with data to proccess
 uploaded_file = st.file_uploader("Choose a file", type=["csv", "xlsx"])
 if uploaded_file is not None:
@@ -39,42 +64,51 @@ if uploaded_file is not None:
     else:
         st.error("Unsupported file type. Please upload a CSV or Excel file.")
         dataframe = None
-st.session_state.input_dataset = dataframe
+    st.session_state.input_dataset = dataframe
+
 if st.session_state.input_dataset is not None:
     st.dataframe(st.session_state.input_dataset)
 
-
+# Configuracion de los modelos
+st.markdown("## Configuración de Modelos")
 tabs_title = [f"Modelo {i+1}" for i in range(st.session_state.num_modelos)]
 tabs = st.tabs(tabs_title)
 for i, tab in enumerate(tabs):
     with tab:
-        st.header(f"Configuración del Modelo {i+1}")
-        st.text_input(f"Nombre del Modelo {i+1}", key=f"model_name_{i}")
-        st.selectbox(
+        st.markdown(f"### Configuración del Modelo {i+1}")
+        model_name = st.text_input(f"Nombre del Modelo {i+1}", key=f"model_name_{i}")
+        model_type = st.selectbox(
             f"Tipo de Modelo {i+1}",
-            ["Regresión", "Clasificación", "Clustering"],
+            model_types,
             key=f"model_type_{i}"
         )
-        st.slider(
-            f"Hiperparámetro 1 del Modelo {i+1}",
-            min_value=0.0,
-            max_value=1.0,
-            value=0.5,
-            step=0.01,
-            key=f"hyperparam_1_{i}"
-        )
-        st.slider(
-            f"Hiperparámetro 2 del Modelo {i+1}",
-            min_value=1,
-            max_value=100,
-            value=10,
-            step=1,
-            key=f"hyperparam_2_{i}"
-        )
+        model = model_factory(model_type)
+        if model is not None:
+            model.set_name(model_name)
+            if st.session_state.input_dataset is not None:
+                model.set_input_df(st.session_state.input_dataset)
+            model.show_ui()
+        st.session_state.models.append(model)
         st.write("Detalles del modelo:")
-        st.json({
-            "Nombre": st.session_state.get(f"model_name_{i}", ""),
-            "Tipo": st.session_state.get(f"model_type_{i}", ""),
-            "Hiperparámetro 1": st.session_state.get(f"hyperparam_1_{i}", 0.5),
-            "Hiperparámetro 2": st.session_state.get(f"hyperparam_2_{i}", 10)
-        })
+        if model is not None:
+            model.model_details()
+        
+        st.markdown("---")
+        st.markdown("### Model prediction")
+        if st.button(f"Predict Model {i+1}") and model is not None and st.session_state.input_dataset is not None:
+            df_pred = model.predict(st.session_state.input_dataset)
+            st.markdown(f"**{model.name}-{model.type}** prediction:")
+            st.dataframe(df_pred)
+
+
+st.markdown("## Realizar predicciones")
+
+if st.button("Models Predict") and st.session_state.input_dataset is not None:
+    models = st.session_state.models
+    input_dataset = st.session_state.input_dataset
+    for m in models:
+        df_pred = m.predict(input_dataset)
+        st.markdown(f"**{m.name}-{m.type}** prediction:")
+        st.dataframe(df_pred)
+
+st.markdown("## Toma de decisiones")
--- settings.py
+++ settings.py
@@ -1,32 +1,200 @@
-class Models:
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+from pandas import DataFrame
+#from sklearn_pandas import DataFrameMapper
 
-    def __init__(self, max_models):
-        self.max_models = max_models
-        self.current_count = 0
-        self.models = []
+import torch
+import torchtuples as tt
+import joblib
 
-    def add_model(self, model):
-        if self.current_count < self.max_models:
-            self.current_count += 1
-            self.models.append(model)
-            return True
-        return False
+from pycox.datasets import metabric
+from pycox.models import CoxPH
+from pycox.evaluation import EvalSurv
 
-    def reset(self):
-        self.current_count = 0
-        self.models = []
+from abc import ABC, abstractmethod
 
-    def remaining(self):
-        return self.max_models - self.current_count
-
-    def is_full(self):
-        return self.current_count >= self.max_models
+class Model(ABC):
+    def __init__(self):
+        self.name = ""
+        self.type = ""
+        self.parameters = {}
+        self.input_df = None
     
-    def get_models(self):
-        return self.models
+    def set_name(self, name):
+        self.name = name
     
-    def get_model(self, index):
-        if 0 <= index < self.current_count:
-            return self.models[index]
-        return None
+    def set_type(self, model_type):
+        self.type = model_type
     
+    def set_parameters(self, parameters):
+        self.parameters = parameters
+    
+    def set_input_df(self, df):
+        self.input_df = df
+    
+    @abstractmethod
+    def show_ui(self)->None:
+        pass
+
+    @abstractmethod
+    def predict(self, dataframe) -> DataFrame:
+        pass
+
+    @abstractmethod
+    def model_details(self)-> None:
+        pass
+
+class DeepSurvModel(Model):
+    instance_count = 0
+    def __init__(self):
+        super().__init__()
+        DeepSurvModel.instance_count += 1
+        self.instance_id = f"deepsurv_{DeepSurvModel.instance_count}"
+        self.type = "DeepSurv"
+        self.net  = tt.practical.MLPVanilla(in_features=45, num_nodes=[32, 32], out_features = 1, batch_norm=True, dropout=0.1, output_bias=False)
+        self.preprocessing_file = None
+        self.state_dict_file = None
+        self.baseline_hazards_file = None
+        if self.instance_id not in st.session_state:
+            st.session_state[self.instance_id] = self
+    
+    def show_ui(self):
+        st.markdown("#### DeepSurv Model Parameters")
+        # Preprocessing file
+        preprocessing_file = st.file_uploader(
+            "Upload preprocessing model file", 
+            accept_multiple_files=False, 
+            type="pkl", 
+            key=f"deepsurv_preprocessing_file_uploader_{self.instance_id}"
+        )
+        if preprocessing_file:
+            st.session_state[f"{self.instance_id}_preprocessing_file"] = preprocessing_file
+            st.success("✅ Preprocessing file uploaded successfully.")
+        elif st.session_state.get(f"{self.instance_id}_preprocessing_file", None):
+            st.info(f"Archivo cargado: {st.session_state[f'{self.instance_id}_preprocessing_file'].name}")
+        self.preprocessing_file = st.session_state.get(f"{self.instance_id}_preprocessing_file", None)
+
+        # State dict file
+        state_dict_file = st.file_uploader(
+            "Upload PyTorch weight models", 
+            accept_multiple_files=False, 
+            type="pt", 
+            key=f"deepsurv_state_dict_file_uploader_{self.instance_id}"
+        )
+        if state_dict_file:
+            st.session_state[f"{self.instance_id}_state_dict_file"] = state_dict_file
+            st.success("✅ PyTorch weight model uploaded successfully.")
+        self.state_dict_file = st.session_state.get(f"{self.instance_id}_state_dict_file", None)
+
+        # Baseline hazards file
+        baseline_hazards_file = st.file_uploader(
+            "Upload baseline hazards file", 
+            accept_multiple_files=False, 
+            type="joblib", 
+            key=f"deepsurv_baseline_hazards_file_uploader_{self.instance_id}"
+        )
+        if baseline_hazards_file:
+            st.session_state[f"{self.instance_id}_baseline_hazards_file"] = baseline_hazards_file
+            st.success("✅ Baseline hazards file uploaded successfully.")
+        self.baseline_hazards_file = st.session_state.get(f"{self.instance_id}_baseline_hazards_file", None)
+    
+    def predict(self, dataframe)->DataFrame:
+        isReady = True
+        if self.state_dict_file is None:
+            st.warning("❗ Please upload PyTorch weight models to continue")
+            isReady = False
+        if self.preprocessing_file is None:
+            st.warning("❗ Please upload preprocessing file to continue.")
+            isReady = False
+        if self.baseline_hazards_file is None:
+            st.warning("❗ Please upload baseline hazards file to continue.")
+            isReady = False
+        if self.input_df is None:
+            st.warning("❗ Please upload input dataset to continue.")
+            isReady = False
+        if not isReady:
+            return pd.DataFrame()
+
+        net = self.net
+        state_dict = torch.load(self.state_dict_file, weights_only=True)
+        net.load_state_dict(state_dict)
+        model_loaded = CoxPH(net)
+        # cargando el baseline hazards 
+        loaded_baseline_hazards = joblib.load(self.baseline_hazards_file)
+        model_loaded.baseline_hazards_ = loaded_baseline_hazards
+        model_loaded.baseline_cumulative_hazards_ = model_loaded.compute_baseline_cumulative_hazards(
+            set_hazards=False, 
+            baseline_hazards_=loaded_baseline_hazards
+        )
+        input_df = self.input_df
+        preprocessor = joblib.load(self.preprocessing_file)
+        X = preprocessor.transform(input_df).astype('float32')
+        surv = model_loaded.predict_surv_df(X)
+        return surv
+    
+    def model_details(self) -> None:
+        st.json({
+            "Name": self.name,
+            "Type": self.type,
+            # "Net": self.net
+            "Preprocessing File": self.preprocessing_file.name if self.preprocessing_file else "Not uploaded",
+        })
+
+class LifelinesCoxPHModel(Model):
+    def __init__(self):
+        super().__init__()
+        self.type = "Lifelines-PHCox"
+    
+    def show_ui(self):
+        st.markdown("### Lifelines CoxPHFitter")
+    
+    def predict(self, dataframe) -> pd.DataFrame:
+        df = pd.DataFrame(np.random.rand(5, 3), columns=['A', 'B', 'C'])
+        return df
+    
+    def model_details(self) -> None:
+        st.json({
+            "Name": self.name,
+            "type": self.type,
+        })
+
+class CoxCCModel(Model):
+    def __init__(self):
+        super().__init__()
+        self.type = "CoxCC"
+    
+    def show_ui(self):
+        st.markdown("### CoxCC")
+    
+    def predict(self, dataframe) -> pd.DataFrame:
+        df = pd.DataFrame(np.random.rand(5, 3), columns=['A', 'B', 'C'])
+        return df
+    
+    def model_details(self) -> None:
+        st.json({
+            "Name": self.name,
+            "type": self.type,
+        })
+
+class CoxTimeModel(Model):
+    def __init__(self):
+        super().__init__()
+        self.type = "CoxTime"
+    
+    def show_ui(self):
+        st.markdown("### CoxTime")
+    
+    def predict(self, dataframe) -> pd.DataFrame:
+        df = pd.DataFrame(np.random.rand(5, 3), columns=['A', 'B', 'C'])
+        return df
+
+    def model_details(self) -> None:
+        st.json({
+            "Name": self.name,
+            "type": self.type,
+        })
+
+
